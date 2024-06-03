0fb2fd151fe65f8b5ec279be8c77962ed525f462
nayan <nayan@begenuin.com>
2024-06-03 06:04:20 +0000

    go init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nayan9229/go-backend-services
+
+go 1.21.9
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"html"
+)
+
+func main() {
+	// Initialize the server
+	server := &http.Server{
+		Addr:    ":9002",
+		Handler: nil, // Set the handler here
+	}
+	http.HandleFunc("/", greet)
+	// Start the server
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Server started")
+	}
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	name := strings.Trim(r.URL.Path, "/")
+	if name == "" {
+		name = "Gopher"
+	}
+
+	fmt.Fprintf(w, "<!DOCTYPE html>\n")
+	fmt.Fprintf(w, "%s, %s!\n", "Hello", html.EscapeString(name))
+}
